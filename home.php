e8fb6a781f33a70fe3abf3e23285bc2b543fe717
Asbudi Anugrah <70067775+stigmaaboutcode@users.noreply.github.com>
2021-09-10 15:54:58 +0800

    Menghapus komentar

--- home.php
+++ home.php
@@ -10,11 +10,7 @@ if($_SESSION['login'] != TRUE){
         <div class="row text-center">
           <!-- sALES FITUR -->
           <?php if($rowakun['status'] == 2 || $rowakun['status'] == 1){ ?>
-         <div class="alert alert-info alert-dismissible fade show" role="alert">
-         <strong>WORK IN PROGRESS! </strong> <br> <span style="font-size:smaller"> MENU DAN FITUR ANALYTICS DAN VIDEO TUTORIAL Sementara kami kembangakan dulu yah kak  <?= $rowakun['username'] ?> 
-           </hr><br>Nanti kami infokan lagi jika sudah siap untuk digunakan.</span>
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>
+         
           <h5>HAI KAK <?= strtoupper($rowakun['username'])  ?></h5>
           <p>Semoga penjualan bulan ini melebihi target yah. Aamiin</p>
           <div class="col-4 col-md-3 mt-3">
